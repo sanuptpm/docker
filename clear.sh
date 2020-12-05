54a33f6d428c82715c7dc47439dd1c6a4f417734
Sanu <sanuptpm@gmail.com>
2020-12-05 18:47:16 +0000

    updated

--- clear.sh
+++ clear.sh
@@ -1,5 +1,17 @@
+
+
+echo "=====stop and delete all contaners======\n"
+for x in $(docker ps -a -q)
+do
+echo "---deleting---$x\n"
+docker stop $x
+docker rm $x
+done
+
+echo "=====remove all images===\n"
 for x in $(docker images -q)
 do
+echo "-----deleting---$x\n"
 docker rmi $x
 done
 
